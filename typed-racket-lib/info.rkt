82a1b852e980056ef44a2df7145a8a5d951e2640
Ryan Newton <rrnewton@gmail.com>
2015-07-09 15:58:02 -0400

    rebase against master and hack base version num to 6.2.0.5

--- typed-racket-lib/info.rkt
+++ typed-racket-lib/info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'multi)
 
-(define deps '(("base" #:version "6.2.0.2")
+(define deps '(("base" #:version "6.2.0.5")
                "pconvert-lib"
                "unstable-contract-lib"
                "unstable-list-lib"
